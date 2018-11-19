23c0c0eb6e63486eccfeb8a22729457968eef8bf
VinhNV <vinhnv@greenglobal.vn>
2018-11-19 08:03:52 +0700

    feat(gg-map) GG map see #136

--- src/constants/config.js
+++ src/constants/config.js
@@ -2,7 +2,10 @@ module.exports = {
   // API_URL: 'http://kiosk.thanhkhe.danang.gov.vn/wp-json/wp/v2',
   // PHP_URL: 'http://kiosk.thanhkhe.danang.gov.vn/wp-search-detail',
   // PORT: 9000
-  API_URL: 'http://192.168.1.150:8080/wp-json/wp/v2',
-  PHP_URL: 'http://192.168.1.150:8080/wp-search-detail',
-  PORT: 8080
+  API_URL: 'http://kiosktk.greenglobal.vn:9977/wp-json/wp/v2',
+  PHP_URL: 'http://kiosktk.greenglobal.vn:9977/wp-search-detail',
+  PORT: 9977
+  // API_URL: 'http://localhost:9977/wp-json/wp/v2',
+  // PHP_URL: 'http://localhost:9977/wp-search-detail',
+  // PORT: 9977
 }
