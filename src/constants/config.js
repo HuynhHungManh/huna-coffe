f28df1066b9f68625ade18f8d711bf41e5f70591
VinhNguyen <vinhnv@greenglobal.vn>
2018-11-26 15:37:25 +0700

    Fix api

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  // API_URL: 'http://kiosk.thanhkhe.danang.gov.vn/wp-json/wp/v2',
-  // PHP_URL: 'http://kiosk.thanhkhe.danang.gov.vn/wp-search-detail',
-  // PORT: 9000
-  API_URL: 'http://kiosktk.greenglobal.vn:9977/wp-json/wp/v2',
-  PHP_URL: 'http://kiosktk.greenglobal.vn:9977/wp-search-detail',
-  PORT: 9977
+  API_URL: 'http://kiosk.thanhkhe.danang.gov.vn/wp-json/wp/v2',
+  PHP_URL: 'http://kiosk.thanhkhe.danang.gov.vn/wp-search-detail',
+  PORT: 9000
+  // API_URL: 'http://kiosktk.greenglobal.vn:9977/wp-json/wp/v2',
+  // PHP_URL: 'http://kiosktk.greenglobal.vn:9977/wp-search-detail',
+  // PORT: 9977
   // API_URL: 'http://localhost:9977/wp-json/wp/v2',
   // PHP_URL: 'http://localhost:9977/wp-search-detail',
   // PORT: 9977
